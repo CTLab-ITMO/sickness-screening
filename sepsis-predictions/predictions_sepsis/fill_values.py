1da7cee6d1a6e63e4a1a01884d141cc045d6f3ef
Artemiy <timtimk30@yandex.ru>
2024-05-31 18:35:41 +0300

    Created train model + fixes
    the function that trains the model is created. Some fixes are done.

--- sepsis-predictions/predictions_sepsis/fill_values.py
+++ sepsis-predictions/predictions_sepsis/fill_values.py
@@ -6,8 +6,9 @@ def fill_values(balanced_csv='balance_need_filling.csv', strategy='most_frequent
     Fills missing values in the dataset using the specified imputation strategy and saves the resulting dataset to a CSV file.
 
     This function reads a dataset, replaces specified placeholders for missing values with NaN, imputes missing values
-    using the specified strategy, and saves the resulting dataset to a CSV file.
-    
+    using the specified strategy, and saves the resulting dataset to a CSV file. If you are using different strategy you should be sure,
+    that all columns in 'balanced_csv' are numeric.
+
     Args:
         balanced_csv (str): Path to the CSV file containing the data to be imputed. Default is 'balance_need_filling.csv'.
         strategy (str): Imputation strategy to use. Options are 'mean', 'median', 'most_frequent', and 'constant'. Default is 'most_frequent'.
